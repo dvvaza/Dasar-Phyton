1978af85e7c7538f963af327f8928d8290984a85
dvvaza <almandadivaza@gmail.com>
2024-02-27 21:00:59 +0700

    my first commit 2024

---
+++ project1-1.py
@@ -0,0 +1,8 @@
+print("Halo guys!!!!!")
+
+nama = "Divaza Almanda"
+umur = "19"
+pekerjaan = "Mahasiswa"
+
+print("Perkenalkan nama aku " + nama + ", saat ini aku berusia " + umur + " dan aku adalah seorang " + pekerjaan)
+print("Terima Kasih!!")
